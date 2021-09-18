f9b046092b92eb95845c3b504b1b3121a0f8c00f
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-18 10:33:52 +0200

    Quoting Strings

---
+++ ex02/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // Only change code below this line
+    var myString = '<a href="http://www.example.com/" target="_blank">Link</a>';
+    // Only change code above this line
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
